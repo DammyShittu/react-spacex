a8bc82fe5d1e850e46971309d1b68589ef01837a
Faizan Zahid <faizan.zahid2500@gmail.com>
2021-10-04 23:02:57 +0500

    Remove unwanted dependencies for now

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Switch } from 'react-router';
+// import { Route, Switch } from 'react-router';
 import Nav from './components/Nav';
 
 const App = () => (
