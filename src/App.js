aa565de45630e248d6b4f7454f04404e61a84600
Faizan Zahid <faizan.zahid2500@gmail.com>
2021-10-04 23:12:02 +0500

    Fix eslint errors

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Nav from './components/Nav';
 
 const App = () => (
   <>
-  <Nav />
+    <Nav />
   </>
 );
 
--- src/index.js
+++ src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
-import App from './App.js';
-
+import App from './App';
 
 ReactDOM.render(
   <React.StrictMode>
     <Router>
-    <App />
+      <App />
     </Router>
   </React.StrictMode>,
   document.getElementById('root'),
