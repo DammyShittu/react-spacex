7584734dc6b54396afd9bac06b22cd27407f03d9
Adedamola Shittu <shittuadedamola@rocketmail.com>
2021-10-04 20:18:16 +0100

    Add NavLink To Navigation Bar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from 'react';
-// import { Route, Switch } from 'react-router';
+import { Route, Switch } from 'react-router';
 import Nav from './components/Nav';
+import Profile from './pages/Profile';
+import Missions from './pages/Missions';
+import Rockets from './pages/Rockets';
 
 const App = () => (
   <>
     <Nav />
+    <Switch>
+      <Route exact path="/myprofile">
+        <Profile />
+      </Route>
+      <Route exact path="/missions">
+        <Missions />
+      </Route>
+      <Route exact path="/">
+        <Rockets />
+      </Route>
+    </Switch>
   </>
 );
 
--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,13 +1,50 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import logo from '../planet.png';
 
-const Nav = () => (
-  <>
-    <nav className="nav-bar">
-      <img src={logo} alt="Space-Ship Logo" />
-      <h3>Space Travelers&apos Hub</h3>
-    </nav>
-  </>
-);
+const Nav = () => {
+  const links = [
+    {
+      id: 1,
+      path: '/',
+      text: 'Rockets',
+    },
+    {
+      id: 2,
+      path: '/missions',
+      text: 'Missions',
+    },
+    {
+      id: 3,
+      path: '/myprofile',
+      text: 'My Profile',
+    },
+  ];
+
+  return (
+    <>
+      <nav className="nav-bar">
+        <div>
+          <img src={logo} alt="Space-Ship Logo" />
+          <h3>Space Travelers&apos; Hub</h3>
+        </div>
+        <ul>
+          {links.map((link) => (
+            <li key={link.id}>
+              <NavLink
+                to={link.path}
+                activeClassName="current"
+                className="nav-link"
+                exact
+              >
+                {link.text}
+              </NavLink>
+            </li>
+          ))}
+        </ul>
+      </nav>
+    </>
+  );
+};
 
 export default Nav;
