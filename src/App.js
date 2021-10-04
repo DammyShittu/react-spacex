13035f691a4bbe317d4fc9613e28bed3185fd4a2
Faizan Zahid <faizan.zahid2500@gmail.com>
2021-10-04 23:02:37 +0500

    Add empty nav bar to the project

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Route, Switch } from 'react-router';
+import Nav from './components/Nav';
+
+const App = () => (
+  <>
+  <Nav />
+  </>
+);
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
+import App from './App.js';
+
 
 ReactDOM.render(
   <React.StrictMode>
-    <h1>Hello</h1>
+    <Router>
+    <App />
+    </Router>
   </React.StrictMode>,
   document.getElementById('root'),
 );
